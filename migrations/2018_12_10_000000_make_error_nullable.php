a29efdf9e196f7f08d4c83b9f48451a2c853d319
David Sevilla Martín <dsevilla192@icloud.com>
2018-12-10 17:09:53 -0500

    Actually fix #3

--- migrations/2018_12_10_000000_make_error_nullable.php
+++ migrations/2018_12_10_000000_make_error_nullable.php
@@ -17,13 +17,13 @@ use Illuminate\Database\Schema\Builder;
 return [
     'up' => function (Builder $schema) {
         $schema->table('webhooks', function (Blueprint $table) {
-            $table->binary('events')->nullable()->change();
+            $table->string('error')->nullable()->change();
         });
     },
 
     'down' => function (Builder $schema) {
         $schema->table('webhooks', function (Blueprint $table) {
-            $table->binary('events')->nullable(false)->change();
+            $table->string('error')->nullable(false)->change();
         });
     },
 ];
