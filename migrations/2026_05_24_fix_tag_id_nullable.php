a4724daf20d020043ea913dfbc6cbfea952d914f
David Sevilla Martin <me@dsevilla.dev>
2026-05-24 12:36:32 -0400

    fix: ensure tag_id column remains nullable after JSON conversion
    This was not caught because some DB backends (eg. MySQL 8.4.2) seem to allow NULL in the JSON field even if it's not marked as nullable. We want to explicitly allow this.

---
+++ migrations/2026_05_24_fix_tag_id_nullable.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of fof/webhooks.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+// Fixes the tag_id column to ensure it remains nullable after the JSON conversion.
+// Looks like some DB backends allow NULL (e.g. MySQL 8.4.2) on JSON fields while other's don't unless explicitly set.
+return [
+    'up' => static function (Builder $schema) {
+        $schema->table('webhooks', function (Blueprint $table) {
+            $table->json('tag_id')->nullable()->change();
+        });
+    },
+    'down' => static function (Builder $schema) {
+        // noop
+    },
+];
