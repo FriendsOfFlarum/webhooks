d4bc694174d46defecfc931aa3379679c8ed0033
David Sevilla Martin <me@dsevilla.dev>
2026-05-24 12:41:28 -0400

    fix: allow null post parameter in getContent method to prevent errors
    1.x ref #85, c51ce53

--- src/Helpers/Post.php
+++ src/Helpers/Post.php
@@ -18,13 +18,17 @@ use Html2Text\Html2Text;
 class Post
 {
     /**
-     * @param \Flarum\Post\Post $post
-     * @param Webhook|null      $webhook
+     * @param \Flarum\Post\Post|null $post
+     * @param Webhook|null $webhook
      *
      * @return string|null
      */
-    public static function getContent(\Flarum\Post\Post $post, ?Webhook $webhook = null): ?string
+    public static function getContent(?\Flarum\Post\Post $post, ?Webhook $webhook = null): ?string
     {
+        if (!$post) {
+            return null;
+        }
+
         $content = $post->content;
 
         if (isset($webhook) && $post instanceof CommentPost) {
